d93c1a23994034465776b3310b7a4a97e4892dcf
joey57 <njorogejoey5@gmail.com>
2022-05-13 15:15:41 +0300

    db initial migration

--- app/models.py
+++ app/models.py
@@ -1,1 +1,18 @@
+from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from datetime import datetime
+
+class User(UserMixin, db.Model):
+  __tablename__ = 'users'
+
+  id = db.Column(db.Integer,primary_key = True)
+  username = db.Column(db.String(255),index = True)
+  email = db.Column(db.String(255), unique = True, index = True)
+  password_hash = db.Column(db.String(255))
+
+  def __repr__(self):
+    return f'User {self.username}'
+   
+
 
