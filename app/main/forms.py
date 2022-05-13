aacc481afc05fe861791840d031117906eae30cc
joey57 <njorogejoey5@gmail.com>
2022-05-13 17:11:19 +0300

    user profile

---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class UpdateProfile(FlaskForm):
+  bio = TextAreaField('Tell us about you', validators=[DataRequired()])
+  submit = SubmitField('Submit')
+  
