1350601112e1d7eacc6d07949528e36faba4bec1
joey57 <njorogejoey5@gmail.com>
2022-05-14 18:24:04 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ class Config:
   '''
   SECRET_KEY='1234'
   SQLALCHEMY_TRACK_MODIFICATIONS = False 
-  SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:whalien52@localhost/blog'
+  # SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://postgres:whalien52@localhost/blog'
   
 class ProdConfig(Config):
   '''
   '''
-  # uri = os.getenv("DATABASE_URL")  
-  # if uri and uri.startswith("postgres://"):
-  #    uri = uri.replace("postgres://", "postgresql://", 1)
-  #    SQLALCHEMY_DATABASE_URI=uri
+  uri = os.getenv("DATABASE_URL")  
+  if uri and uri.startswith("postgres://"):
+     uri = uri.replace("postgres://", "postgresql://", 1)
+     SQLALCHEMY_DATABASE_URI=uri
   pass
   
  
