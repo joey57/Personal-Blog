d7eb07f5ade1f5a48a46f8f00cceca8e47d59d0a
joey57 <njorogejoey5@gmail.com>
2022-05-16 10:10:20 +0300

    deploy

--- config.py
+++ config.py
@@ -9,15 +9,10 @@ class Config:
 class ProdConfig(Config):
   '''
   '''
-  # uri = os.getenv("DATABASE_URL")  
-  # if uri and uri.startswith("postgres://"):
-  #    uri = uri.replace("postgres://", "postgresql://", 1)
-  #    SQLALCHEMY_DATABASE_URI=uri
-  pass
-  
-  
-  
- 
+  uri = os.getenv("DATABASE_URL")  
+  if uri and uri.startswith("postgres://"):
+     uri = uri.replace("postgres://", "postgresql://", 1)
+     SQLALCHEMY_DATABASE_URI=uri
 
 class DevConfig(Config):
   '''
